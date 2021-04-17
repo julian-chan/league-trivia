fd01c6802ab204be7476e36e555db903d8c65f66
Julian Chan <julianchan0928@gmail.com>
2021-04-16 17:57:01 -0700

    configured app's static folder to point to frontend build

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask
 from flask_restful import Api
 from flask_api.LeagueTriviaAPI import ChampionsAPI, SimilarChampionPassivesByColorAPI, SpellsAPI, SimilarSpellsByColorAPI, ItemsAPI, SimilarItemsByTagsAPI, RunesAPI, QuestionAPI
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
 api = Api(app)
 # CORS(app)
+
+@app.route('/')
+def index():
+  return app.send_static_file('index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+  return app.send_static_file('index.html')
 
 api.add_resource(ChampionsAPI, '/api/champions')
 api.add_resource(SimilarChampionPassivesByColorAPI, '/api/similarPassives')
@@ -17,4 +25,4 @@ api.add_resource(RunesAPI, '/api/runes')
 api.add_resource(QuestionAPI, '/api/question')
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host='0.0.0.0', ddebug=False, port=os.environ.get('PORT', 80))
